98d260da7c5b6396d82c4088640e4a19d4ae264b
oheart <2353769123@qq.com>
2018-12-17 18:24:44 +0800

    类

--- greeter.js
+++ greeter.js
@@ -1,48 +1,16 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Animal = /** @class */ (function () {
-    function Animal(theName) {
-        this.name = theName;
+var Grid = /** @class */ (function () {
+    function Grid(scale) {
+        this.scale = scale;
     }
-    Animal.prototype.move = function (distanceInMeters) {
-        if (distanceInMeters === void 0) { distanceInMeters = 0; }
-        console.log(this.name + " moved " + distanceInMeters + "m.");
+    Grid.prototype.calculateDistanceFromOrigin = function (point) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
     };
-    return Animal;
+    Grid.origin = { x: 0, y: 0 };
+    return Grid;
 }());
-var Snake = /** @class */ (function (_super) {
-    __extends(Snake, _super);
-    function Snake(name) {
-        return _super.call(this, name) || this;
-    }
-    Snake.prototype.move = function (distanceInMeters) {
-        if (distanceInMeters === void 0) { distanceInMeters = 5; }
-        console.log('Slithering...');
-        _super.prototype.move.call(this, distanceInMeters);
-    };
-    return Snake;
-}(Animal));
-var Horse = /** @class */ (function (_super) {
-    __extends(Horse, _super);
-    function Horse(name) {
-        return _super.call(this, name) || this;
-    }
-    Horse.prototype.move = function (distanceInMeters) {
-        if (distanceInMeters === void 0) { distanceInMeters = 45; }
-        console.log("Galloping...");
-        _super.prototype.move.call(this, distanceInMeters);
-    };
-    return Horse;
-}(Animal));
-var sam = new Snake('Sammy the Python');
-var tom = new Horse("Tommy the Palomino");
-sam.move();
-tom.move(34);
+var grid1 = new Grid(1.0); // 1x scale
+var grid2 = new Grid(5.0); // 5x scale
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
